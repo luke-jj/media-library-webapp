92994d9a042efbbc5f9f912314446d463b147128
LukeJ <infiLuca@gmail.com>
2019-02-19 18:28:03 +0100

    add data formatting

--- inc/functions.php
+++ inc/functions.php
@@ -6,4 +6,25 @@ function get_item_html($id, $item) {
         . "<p>View Details</p>"
         . "</a></li>";
 }
+
+function array_category($catalog, $category) {
+  if ($category == null) {
+    return array_keys($catalog);
+  }
+
+  $output = array();
+
+  foreach($catalog as $id => $item) {
+    if ($category == null OR strtolower($category) == strtolower($item["category"])) {
+      $sort = $item["title"];
+      $sort = ltrim($sort, "The ");
+      $sort = ltrim($sort, "A ");
+      $sort = ltrim($sort, "An ");
+      $output[$id] = $sort;
+    }
+  }
+
+  asort($output);
+  return array_keys($output);
+}
 ?>
